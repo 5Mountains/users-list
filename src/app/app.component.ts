c2c6925b856a7feccbc3be2e6988d5402c8cb9da
5Mountains <V.Piatyhor@gmail.com>
2020-08-30 14:02:31 +0300

    updated readme
    updated readme file and a little style and appearance.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'users-list';
+  title = 'Users List';
   typesOfShoes: string[] = ['Smith', 'Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
 }
