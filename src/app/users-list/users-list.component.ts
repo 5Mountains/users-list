c7cb2a3b0fdd7ff13b3444d3a7c00ba39be66ab3
5Mountains <V.Piatyhor@gmail.com>
2020-08-30 10:53:21 +0300

    added removal feature
    the deletion function was added by me

--- src/app/users-list/users-list.component.ts
+++ src/app/users-list/users-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from './../users.service';
+import { MatListOption } from '@angular/material/list';
+import { User } from '../shared/user';
+
 
 @Component({
   selector: 'app-users-list',
@@ -8,7 +11,11 @@ import { UsersService } from './../users.service';
 })
 export class UsersListComponent implements OnInit {
 
-  usersList = [];
+  usersList: User[] = [];
+  username: string;
+  name: string;
+  role: string;
+  selectedList: User[];
 
   constructor(
     public userService: UsersService
@@ -24,6 +31,32 @@ export class UsersListComponent implements OnInit {
 
   sort(direction: string){
     this.usersList = this.userService.sortUsers(direction);
+  }
+
+  addUser(){
+    this.userService.addUser({
+      id: Math.floor((Math.random() * 6) + 10),
+      name: this.name,
+      username: this.username,
+      email: "",
+      role: this.role,
+      phone: "",
+      website: ""
+    });
+    this.usersList = this.userService.getUsersList();
+  }
+
+  selectItem(users: MatListOption[]){
+    this.selectedList = [];
+    users.forEach(element => {
+      this.selectedList.push(element.value);
+    });
+  }
+
+  deleteUsers(){
+    this.userService.deleteUsers(this.selectedList);
+    this.usersList = this.userService.getUsersList();
+
   }
 
 }
